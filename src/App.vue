0d0e08eddd1d9ae1c802d5eea020ff0a077c0486
w ell <3096250047@qq.com>
2024-02-27 16:10:37 +0800

    feat:新增开关侧边栏和调试器的功能

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header><navBS></navBS></el-header>
+      <el-header>
+        <navBS></navBS>
+      </el-header>
       <el-container>
-        <el-aside width="200px">
+        <el-aside :width="isShow ? '200px' : '0'">
           <asideBS></asideBS>
         </el-aside>
         <el-container>
           <el-main>
             <homeBS></homeBS>
           </el-main>
-          
+
         </el-container>
       </el-container>
       <el-footer>
@@ -26,6 +28,8 @@ import footerBS from './components/footerBS.vue'
 import navBS from './components/navBS.vue'
 import asideBS from './components/asideBS.vue'
 import homeBS from './page/homeBS.vue'
+import { useStore } from 'vuex'
+import { computed } from 'vue'
 
 export default {
   name: 'App',
@@ -34,24 +38,34 @@ export default {
     navBS,
     asideBS,
     homeBS
+  },
+  setup() {
+    const store = useStore()
+    let isShow = computed(() => {
+      return store.getters['global/isShowAside']
+    })
+    return {
+      isShow
+    }
   }
 }
 </script>
 
 <style>
-
-*{
+* {
   transition: 0.5s;
   /* background-color: black; */
 }
+
 .el-main {
   padding: 0;
 }
+
 .el-header {
   padding: 0;
 }
+
 .el-footer {
   padding: 0;
 }
-
 </style>
--- src/page/homeBS.vue
+++ src/page/homeBS.vue
@@ -1,21 +1,21 @@
 <!-- 工作台组件 -->
 <template>
   <div class="workheader">
-    {{pageTitle}}
+    {{ pageTitle }}
   </div>
 
-  <el-divider/>
+  <el-divider />
 
   <div class="workspace">
     <div class="left-panel">
       <CodeEditorBS></CodeEditorBS>
     </div>
-    
+
     <div class="right-panel">
       <CompilerBS></CompilerBS>
-      <DebuggerBS></DebuggerBS>
+      <DebuggerBS v-if="isShow"></DebuggerBS>
     </div>
-    
+
   </div>
 </template>
 
@@ -23,44 +23,53 @@
 import CodeEditorBS from '@/workSpace/CodeEditorBS.vue'
 import CompilerBS from '@/workSpace/CompilerBS.vue'
 import DebuggerBS from '@/workSpace/DebuggerBS.vue'
-import {useStore} from 'vuex'
+import { useStore } from 'vuex'
 import { computed } from 'vue'
 
 export default {
-    name:"homeBS",
-    components:{
-      CodeEditorBS,
-      CompilerBS,
-      DebuggerBS
-    },
-    setup(){
-      const store = useStore()
-      let pageTitle = computed(()=>{
-        const selectedFile = store.getters['files/selectedFile']
-        return selectedFile? selectedFile.name:'未选中文件'
-      })
-      return {
-        pageTitle
-      }
+  name: "homeBS",
+  components: {
+    CodeEditorBS,
+    CompilerBS,
+    DebuggerBS
+  },
+  setup() {
+    const store = useStore()
+    let pageTitle = computed(() => {
+      const selectedFile = store.getters['files/selectedFile']
+      return selectedFile ? selectedFile.name : '未选中文件'
+    })
+    let isShow = computed(() => {
+      return store.getters['global/isShowDebugger']
+    })
+    return {
+      pageTitle,
+      isShow
     }
+  }
 }
 </script>
 
 <style scoped>
-*{
+* {
   overflow: hidden;
 }
+
 .workspace {
   flex: 1;
-  background-color: #333; /* 设置为暗色背景，可以根据需要调整颜色 */
-  color: #fff; /* 设置文本颜色为白色，以适应暗色背景 */
+  background-color: #333;
+  /* 设置为暗色背景，可以根据需要调整颜色 */
+  color: #fff;
+  /* 设置文本颜色为白色，以适应暗色背景 */
   padding: 10px;
-  overflow-y: auto; /* 添加滚动条，以适应内容溢出时的滚动 */
+  overflow-y: auto;
+  /* 添加滚动条，以适应内容溢出时的滚动 */
   height: 95vh;
   display: flex;
-  
+
 }
-.workheader{
+
+.workheader {
   /* padding: 0px; */
   height: 5vh;
   background-color: rgba(51, 51, 51, 0.8);
@@ -69,20 +78,24 @@ export default {
   justify-content: center;
   color: #fff;
   font-size: 1.5rem;
-  border-bottom: 2px solid #333; /* 添加底部边框 */
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2); 
+  border-bottom: 2px solid #333;
+  /* 添加底部边框 */
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
 
 }
-.el-divider--horizontal{
+
+.el-divider--horizontal {
   margin: 0;
   height: 0;
   /* color: rgb(108, 37, 37); */
-  border-top:rgba(51, 51, 51, 0.687)
+  border-top: rgba(51, 51, 51, 0.687)
 }
-.left-panel{
+
+.left-panel {
   flex: 1;
 }
-.right-panel{
+
+.right-panel {
   flex: 1;
   display: flex;
   flex-direction: column;
