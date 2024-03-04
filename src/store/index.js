75f1b848b7fa61c5d6c76f2f9b7d3321cbd482c0
kurtDubain <kurt.du.cobain@gmail.com>
2024-03-04 16:23:10 +0800

    feat:新增用于维护当前文件断点位置的数组

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import { createStore } from "vuex";
 // 引入modules
 import filesModule from './modules/files'
 import globalModule from './modules/global'
+import debugModule from './modules/debug'
 
 
 const store = createStore({
-    modules:{
-        files:filesModule,
-        global:globalModule
+    modules: {
+        files: filesModule,
+        global: globalModule,
+        debug: debugModule
     }
 })
 
