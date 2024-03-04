93ea27ad65ac998cf67f04a02b232896e7cdb4e0
kurtDubain <kurt.du.cobain@gmail.com>
2024-03-04 16:23:32 +0800

    feat:新增添加断点功能

--- src/workSpace/CodeEditorBS.vue
+++ src/workSpace/CodeEditorBS.vue
@@ -21,24 +21,68 @@ export default {
     // 初始化所选中的文件的内容
     let code = computed(() => store.getters['files/selectedFile'])
     let editorCode = ref(null)//编辑器容器
-    let editorCodeContent = ref(null)//编辑器实例
-
+    let editorCodeContent = null//编辑器实例
+    let debugRowIds = computed(() => store.getters['debug/rowIds'])//断点行号
+    const decorationIds = [];
     const initEditor = async () => {
       await nextTick(); // Wait for the next DOM update
-
-      editorCodeContent.value = monaco.editor.create(editorCode.value, {
+      editorCodeContent = monaco.editor.create(editorCode.value, {
         value: code.value ? code.value.content : '请先新建文件~',
         language: 'pascal',
         theme: 'vs-dark',
         readOnly: true,
+        glyphMargin: true,
       });
       monaco.languages.register({ id: 'pascal' })
       monaco.languages.setMonarchTokensProvider('pascal', pascalLanguageConfig)
       monaco.languages.registerCompletionItemProvider('pascal', pascalCompletionProvider)
       //编辑代码的时候，同步修改所选中文件的内容
-      editorCodeContent.value.onDidChangeModelContent(() => {
+      editorCodeContent.onDidChangeModelContent(() => {
         if (!code.value) return
-        code.value.content = toRaw(editorCodeContent.value).getValue()
+        code.value.content = toRaw(editorCodeContent).getValue()
+      })
+      editorCodeContent.onMouseDown(e => {
+        if (!code.value) return
+        if (![2, 3].includes(e.target.type)) return
+        const position = e.target.position;
+        const lineDecoration = editorCodeContent.getModel().getLineDecorations(position.lineNumber);
+        let flagId = "";
+        lineDecoration.forEach((decoration) => {
+          if (decoration.options.marginClassName === "myGlyphMarginClass") {
+            decorationIds.push(decoration.id);
+            flagId = decoration.id;
+          }
+        });
+        if (decorationIds.length === 0) {
+          //新增样式
+          const range = {
+            startLineNumber: position.lineNumber,
+            startColumn: 1,
+            endLineNumber: position.lineNumber,
+            endColumn: 1,
+          };
+          const breakPointDec = {
+            range,
+            options: {
+              isWholeLine: true,
+              className: "contentClass",
+              marginClassName: "myGlyphMarginClass",
+              stickiness:
+                monaco.editor.TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
+            },
+          };
+          store.commit('debug/addRowId', position.lineNumber);
+          console.log(position.lineNumber);
+          console.log(debugRowIds.value);
+          editorCodeContent.getModel().deltaDecorations([], [breakPointDec]);
+        } else {
+          //删除
+          const index = decorationIds.findIndex((id) => id === flagId);
+          editorCodeContent.getModel().deltaDecorations(decorationIds, []);
+          decorationIds.splice(index, 1);
+          store.commit('debug/deleteRowId', position.lineNumber);
+          console.log(debugRowIds.value);
+        }
       })
 
     }
@@ -52,15 +96,17 @@ export default {
     // editorCode.value = code.value
     // 当切换文件的时候，更新code内容
     eventBus.on('changeFile', () => {
+      store.commit('debug/clearRowIds');
+      console.log(debugRowIds);
       // editorCode.value = code.value.content
-      editorCodeContent.value.updateOptions({
+      editorCodeContent.updateOptions({
         readOnly: false
       });
       if (code.value) {
-        toRaw(editorCodeContent.value).setValue(code.value.content);
+        toRaw(editorCodeContent).setValue(code.value.content);
       } else {
-        toRaw(editorCodeContent.value).setValue('请先新建文件~');
-        editorCodeContent.value.updateOptions({
+        toRaw(editorCodeContent).setValue('请先新建文件~');
+        editorCodeContent.updateOptions({
           readOnly: true
         });
       }
@@ -97,6 +143,15 @@ export default {
 .code-editor .monaco-editor {
   height: 100% !important;
   width: 100% !important;
+}
+
+.myGlyphMarginClass {
+  top: 4px !important;
+  left: 10px !important;
+  width: 10px !important;
+  height: 10px !important;
+  background: red !important;
+  border-radius: 50%;
 }
 </style>
   
