1a2a4fa002dec1c5965be59786b97f87912d4b0b
kurtDubain <kurt.du.cobain@gmail.com>
2024-03-05 10:37:04 +0800

    feat:新增调试器初始化功能

--- src/api/modules/debugger.js
+++ src/api/modules/debugger.js
@@ -1,25 +1,25 @@
 import api from "../index";
 // 获取调试信息
 export const getDebugInfo = (programState) => {
-  return api.post("/debug/info", { programState });
+  return api.post("/debugger/info", { programState });
 };
 // 设置断点
 export const setBreakpoint = (lineNumber) => {
-  return api.post("/debug/breakpoint", { lineNumber });
+  return api.post("/debugger/breakpoint", { lineNumber });
 };
 // 继续执行
 export const continueExecution = () => {
-  return api.post("/debug/continue");
+  return api.post("/debugger/continue");
 };
-// 单步执行
-export const stepOver = () => {
-  return api.post("/debug/step-over");
+// 初始化
+export const init = (data) => {
+  return api.post("/debugger/init", { data });
 };
 // 逐句执行
 export const stepInto = () => {
-  return api.post("/debug/step-into");
+  return api.post("/debugger/step-into");
 };
 // 跳出当前函数
 export const stepOut = () => {
-  return api.post("/debug/step-out");
+  return api.post("/debugger/step-out");
 };
