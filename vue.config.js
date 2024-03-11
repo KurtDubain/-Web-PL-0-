0f68981cd6db075fa5d8bd00772cbb122f67bd2a
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-03-11 20:02:38 +0800

    更新了更新日志

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   css: {
     loaderOptions: {
       sass: {
-        implementation: require('sass'),
+        implementation: require("sass"),
       },
     },
   },
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://www.dyp02.vip:3001',
+      "/api": {
+        // target: 'http://www.dyp02.vip:3001',
+        target: "http://localhost:3001",
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
-})
+          "^/api": "",
+        },
+      },
+    },
+  },
+});
