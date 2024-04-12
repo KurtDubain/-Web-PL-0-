5de75bd5b1772bc16df760aa36929c4bb8baa33a
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-04-12 09:43:02 +0800

    fix:更新了初始路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import AboutRoutes from "./modules/about";
 const routes = [...HomeRoutes, ...AboutRoutes];
 
 const router = createRouter({
-  history: createWebHistory("/web-pl0"),
+  history: createWebHistory(""),
   routes,
 });
 
