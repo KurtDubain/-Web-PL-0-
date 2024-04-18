099f53f8d04cf434e1f56a85a8b4253e6b5130d6
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-04-18 18:48:17 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,3 +17,22 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 app.mount("#app");
+const debounce = (fn, delay) => {
+  let timer
+   return (...args) => {
+     if (timer) {
+       clearTimeout(timer)
+     }
+     timer = setTimeout(() => {
+       fn(...args)
+     }, delay)
+   }
+}
+  
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver{
+   constructor(callback) {
+     callback = debounce(callback, 200);
+     super(callback);
+   }
+}
