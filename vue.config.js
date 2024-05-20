b7c9cce0fa30ffecdd3e8d353d0eb923a2fc1aa0
xupeng.du <kurt.du.cobain@gmail.com>
2024-05-20 10:51:04 +0800

    fix：看起来并没有修改什么，匹配了服务端版本

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,9 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "/api": {
-        target: "http://www.dyp02.vip:3001",
+        // target: "http://www.dyp02.vip:3001",
         // target: "http://localhost:3001",
+        target: "https://www.dyp02.vip:8443",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "",
